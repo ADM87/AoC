3a96c306e1de2af50c20351f050d7ed8ab5e71a1
ADM87 <aaron.d.malley@gmail.com>
2024-12-05 19:47:29 -0400

    Added comments to day 5

--- 2024/Day5/main.go
+++ 2024/Day5/main.go
@@ -8,11 +8,13 @@ import (
 	"strings"
 )
 
+// SortRule represents a rule for sorting pages.
 type SortRule struct {
-	Page     string
-	Proceeds map[string]bool
+	Page     string          // The page number.
+	Proceeds map[string]bool // A map of pages that this page proceeds.
 }
 
+// NewSortRule creates a new SortRule with the given page number and order.
 func NewSortRule(page, order string) *SortRule {
 	return &SortRule{
 		Page:     page,
@@ -47,45 +49,59 @@ func main() {
 	fmt.Println(totalMiddle(ordered, updated))
 }
 
-func totalMiddle(guides [][]string, updated []int) int {
+// totalMiddle returns the sum of the middle values of the guides at the given indices.
+func totalMiddle(guides [][]string, indices []int) int {
 	total := 0
-	for _, i := range updated {
+	// Iterate over the indices and sum the middle values of the guides.
+	for _, i := range indices {
 		if i < 0 || i >= len(guides) {
 			continue
 		}
 
+		// Find the middle value of the guide.
 		m := len(guides[i]) / 2
 		fmt.Println("Middle:", guides[i][m])
 
+		// Convert the middle value to an integer and add it to the total.
 		v, err := strconv.Atoi(guides[i][m])
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
+		// Add the middle value to the total.
 		total += v
 	}
 	return total
 }
 
+// orderGuides orders the guide pages based on the rules.
 func orderGuides(input string, rules map[string]*SortRule) ([][]string, []int, []int) {
-	ordered := [][]string{}
-	updated := map[int]bool{}
+	ordered := [][]string{}   // A new list of sorted guides.
+	updated := map[int]bool{} // Tracks the indices of guides that have been updated.
 
 	fmt.Println("Guides:")
-	for g, guide := range getPageSequences(input) {
+	for g, guide := range getPageGroups(input) {
 		fmt.Println(guide)
 
 		updated[g] = false
+
+		// Iterate over the guide pages twice.
+		// The first iteration is to find the pages that need to be swapped.
+		// The second iteration is to swap the pages.
 		for i := 0; i < len(guide); i++ {
 			for j := i + 1; j < len(guide); j++ {
+				// If the page at index i proceeds the page at index j, swap the pages.
 				if rule, ok := rules[guide[j]]; ok && rule.Proceeds[guide[i]] {
+					// i proceeds j, swap the pages.
 					guide[i], guide[j] = guide[j], guide[i]
+					// Mark the guide as updated.
 					updated[g] = true
 				}
 			}
 		}
 
+		// Append the sorted guide to the ordered list.
 		ordered = append(ordered, guide)
 	}
 
@@ -96,8 +112,8 @@ func orderGuides(input string, rules map[string]*SortRule) ([][]string, []int, [
 		}
 	}
 
-	updatedIndices := []int{}
-	correctIndices := []int{}
+	updatedIndices := []int{} // Holds all the indices of the guides that were updated.
+	correctIndices := []int{} // Holds all the indices of the guides that were not updated.
 
 	for i, v := range updated {
 		if v {
@@ -110,9 +126,13 @@ func orderGuides(input string, rules map[string]*SortRule) ([][]string, []int, [
 	return ordered, correctIndices, updatedIndices
 }
 
+// createRules builds a map of SortRules from the input string.
+// Each SortRule contains a page number and all the pages that it proceeds.
 func createRules(input string) map[string]*SortRule {
 	rules := map[string]*SortRule{}
 	for _, line := range strings.Split(input, "\n") {
+		// Split the line on the pipe: Ex "75|93" -> ["75", "93"]
+		// The first element is the page number and the second element is the page it proceeds.
 		sp := strings.Split(line, "|")
 		if r := rules[sp[0]]; r == nil {
 			rules[sp[0]] = NewSortRule(sp[0], sp[1])
@@ -123,11 +143,14 @@ func createRules(input string) map[string]*SortRule {
 	return rules
 }
 
-func getPageSequences(input string) [][]string {
+// getPageGroups returns a slice of slices of strings representing the the groups of pages.
+func getPageGroups(input string) [][]string {
 	pages := [][]string{}
 
 	for _, line := range strings.Split(input, "\n") {
+		// Split the line on the comma: Ex "75,93",45,23" -> ["75", "93", "45", "23"]
 		sp := strings.Split(line, ",")
+		// If the split line is empty, skip it.
 		if sp[0] == "" {
 			continue
 		}
@@ -137,6 +160,9 @@ func getPageSequences(input string) [][]string {
 	return pages
 }
 
+// loadInput reads the input file and returns a slice of strings representing the input data.
+// Splits the input on the double newline character: \n\n
+// The first element is the rules and the second element is the guides.
 func loadInput(fileName string) []string {
 	report := ""
 
